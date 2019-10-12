ce266ab9686b276004a3fc46d4e919a20e79d35a
2B-people <nqq0915@163.com>
2019-10-12 17:24:31 +0800

    Link_list some thing todo

--- day_file/c_cpp/cpp_learn/Link_List.cpp
+++ day_file/c_cpp/cpp_learn/Link_List.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include "one_way_test.h"
+#include "Link_List.h"
+#include <stddef.h>
 
 using namespace std;
 
 template <typename T>
-OneWayList<T>::OneWayList()
+OneWayList<T>::OneWayList() : size_(0)
 {
     phand_ = new ListNode<T>;
     if (phand_ == nullptr)
@@ -15,10 +16,10 @@ OneWayList<T>::OneWayList()
 }
 
 template <typename T>
-OneWayList<T>::OneWayList(T first_data)
+OneWayList<T>::OneWayList(T first_data) : size_(1)
 {
     phand_ = new ListNode<T>;
-    
+
     if (phand_ == nullptr)
     {
         cout << "error:Can't create hand" << endl;
@@ -38,6 +39,18 @@ OneWayList<T>::~OneWayList()
 }
 
 template <typename T>
+size_t OneWayList<T>::GetSize()
+{
+    return size_;
+}
+
+template <typename T>
+ListNode<T> *OneWayList<T>::GetpHand()
+{
+    return phand_;
+}
+
+template <typename T>
 bool OneWayList<T>::Insert(T in_data)
 {
     ListNode<T> *insert_node = new ListNode<T>;
@@ -53,20 +66,56 @@ bool OneWayList<T>::Insert(T in_data)
     if (phand_->next == nullptr)
     {
         phand_->next = insert_node;
+        size_++;
         return true;
     }
     else
     {
         insert_node->next = phand_->next;
         phand_->next = insert_node;
+        size_++;
         return true;
     }
 }
 
 template <typename T>
-bool OneWayList<T>::InsertSort(T in_data)
+bool OneWayList<T>::InsertSort(const T in_data)
 {
-    
+    ListNode<T> *insert_node = new ListNode<T>;
+    if (insert_node == nullptr)
+    {
+        cout << "error:Can't insert node" << endl;
+        return false;
+    }
+
+    insert_node->data = in_data;
+    insert_node->next = nullptr;
+
+    if (phand_->next == nullptr)
+    {
+        phand_->next = insert_node;
+        size_++;
+        return true;
+    }
+
+    ListNode<T> *index_node = phand_;
+    while (index_node->next != nullptr)
+    {
+        if (insert_node->data <= index_node->next->data
+             && insert_node->data >= index_node->data)
+        {
+            ListNode<T> *temp = index_node->next;
+            index_node->next = insert_node;
+            insert_node->next = temp;
+            size_++;
+            return true;
+        }
+        index_node = index_node->next;
+    }
+
+    index_node->next = insert_node;
+    size_++;
+    return true;
 }
 
 template <typename T>
@@ -78,18 +127,21 @@ ListNode<T> *OneWayList<T>::Find(T find_data)
         return nullptr;
     }
 
-    ListNode<T> *index_node = phand_;
-    while (index_node->next != nullptr)
+    ListNode<T> *index_node = phand_->next;
+    while (index_node != nullptr)
     {
-        if (index_node->data == find_data)
+        if (abs(find_data - index_node->data) <= 0.01)
+        {
             return index_node;
+        }
+        index_node = index_node->next;
     }
     cout << "warning: Can't not find " << find_data << endl;
     return nullptr;
 }
 
 template <typename T>
-bool Delete(T del_data)
+bool OneWayList<T>::Delete(T del_data)
 {
     if (phand_->next == nullptr)
     {
@@ -102,6 +154,7 @@ bool Delete(T del_data)
         {
             delete phand_->next;
             phand_->next = nullptr;
+            size_--;
             return true;
         }
         else
@@ -115,13 +168,59 @@ bool Delete(T del_data)
     {
         if (index_node->next->data == del_data)
         {
-            ListNode<T> *temp = index_node->next->next;
+            ListNode<T> *temp = index_node->next;
             delete index_node->next;
             index_node->next = temp;
+            size_--;
             return true;
         }
+        index_node = index_node->next;
     }
 
-    cout << "warning: can't find data what to del " << find_data << endl;
+    cout << "warning: can't find data what to del " << endl;
     return false;
+}
+
+template <typename T>
+void OneWayList<T>::Destroy()
+{
+    
+}
+
+template <typename T>
+void OneWayList<T>::Sort()
+{
+
+}
+
+template <typename T>
+void OneWayList<T>::Print()
+{
+    ListNode<T> *index_node = phand_->next;
+    while (index_node != nullptr)
+    {
+        cout << index_node->data << endl;
+        index_node = index_node->next;
+    }
+    return;
+}
+
+int main(int argc, char const *argv[])
+{
+    OneWayList<double> test_list;
+
+    test_list.InsertSort(81.1);
+    test_list.InsertSort(32.1);
+    test_list.InsertSort(10.1);
+    test_list.InsertSort(10.1);
+    test_list.InsertSort(9.1);
+    test_list.InsertSort(12.1);
+
+    test_list.Print();
+
+    ListNode<double> *aa = test_list.Find(9.1);
+    if (aa != nullptr)
+        cout << "test find:" << aa->data << endl;
+
+    return 0;
 }
--- day_file/c_cpp/cpp_learn/Link_List.h
+++ day_file/c_cpp/cpp_learn/Link_List.h
@@ -1,25 +1,30 @@
 #ifndef __LINK_LIST_H__
 #define __LINK_LIST_H__
 
-template<typename T>
+#include <stddef.h>
+
+template <typename T>
 struct ListNode
 {
     T data;
     ListNode<T> *next;
 };
 
-template<typename T>
+template <typename T>
 class OneWayList
 {
 public:
     OneWayList();
     OneWayList(T first_data);
 
-   ~OneWayList();
+    ~OneWayList();
+
+    size_t GetSize();
+    ListNode<T> *GetpHand();
 
     bool Insert(T in_data);
     bool InsertSort(T in_data);
-    ListNode<T>* Find(T find_data);
+    ListNode<T> *Find(T find_data);
     bool Delete(T del_data);
 
     void Destroy();
@@ -28,9 +33,7 @@ public:
 
 private:
     ListNode<T> *phand_;
+    size_t size_;
 };
-
-
-
 
 #endif //__LINK_LIST_H__
